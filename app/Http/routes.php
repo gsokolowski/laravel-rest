47ae398afba063c957dac2f1bce47a1df8083f2f
gsokolowski <gregor.sokolowski@gmail.com>
2016-02-22 14:31:32 +0000

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,39 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    //return $environment = App::environment();
+});
+
+Route::get('users', 'TokenAuthController@index');
+
+Route::post('api/register', 'TokenAuthController@register');
+Route::post('api/authenticate', 'TokenAuthController@authenticate');
+Route::get('api/authenticate/user', 'TokenAuthController@getAuthenticatedUser');
+
+Route::resource('api/todo', 'TodoController');
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
